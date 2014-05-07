ac90b79b14e76ff58785854756ed61c116e63fd3
Tak Tran <contact@tutaktran.com>
2014-05-07 18:02:36 +0100

    Debounce is now in setWatch. Update button file to use it, and simplify functions.

--- bin/espruino/button.js
+++ bin/espruino/button.js
@@ -4,19 +4,14 @@
  * Increment a counter when the button is pressed
  */
 var pressCount = 0;
-var lastPress = 0;
 
-function onPress(timeDiff) {
+function buttonWatcher(e) {
   pressCount++;
   console.log(pressCount);
 }
 
-function buttonWatcher(e) {
-  var timeDiff = e.time - lastPress;
-  lastPress = e.time;
-  if (timeDiff > 0.1) {
-    onPress(timeDiff);
-  }
-}
-
-setWatch(buttonWatcher, BTN, {edge:"falling", repeat:true});
+setWatch(buttonWatcher, BTN, {
+  edge: "falling",
+  repeat: true,
+  debounce: 100
+});
