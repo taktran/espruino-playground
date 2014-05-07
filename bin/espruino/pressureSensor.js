ff1bda41c752a866c45c49ab024f9a45eb6aeabb
Tak Tran <contact@tutaktran.com>
2014-05-07 17:59:26 +0100

    Add pressure sensor example.

---
+++ bin/espruino/pressureSensor.js
@@ -0,0 +1,18 @@
+"use strict";
+
+/**
+ * Pressure sensor
+ */
+
+var PRESSURE_SENSOR = A9;
+var counter = 0;
+
+function pressureSensorWatcher(e) {
+  counter++;
+  console.log("Press " + counter);
+}
+
+setWatch(pressureSensorWatcher, PRESSURE_SENSOR, {
+  repeat: true,
+  debounce: 100
+});
